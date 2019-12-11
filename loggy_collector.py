54d826f4e8dc0df1519c5eff1df35ad096ecd9c2
best2000 <silentman@hotmail.co.th>
2019-12-12 03:50:40 +0700

    lol

--- loggy_collector.py
+++ loggy_collector.py
@@ -1,20 +1,6 @@
+import pickle, re
 from class_all import *
-import pickle
-import re
-import time
-
-def loggy_add(object): #add ip object to database
-    with open('class_loggy_obj.pickle', 'ab') as file:
-        pickle.dump(object, file)
-
-def loggy_rewrite(loggy_list):
-    for i in range(len(loggy_list)): #replace object in dummy list to database file
-        if i == 0:
-            with open('class_loggy_obj.pickle', 'wb') as file:
-                pickle.dump(loggy_list[i], file)
-        else:
-            with open('class_loggy_obj.pickle', 'ab') as file:
-                pickle.dump(loggy_list[i], file)
+from ip_collector import *
 
 def loggy_show_info_all(): #show all loggy object in database
     with open('class_loggy_obj.pickle', 'rb') as file:     
@@ -24,6 +10,10 @@ def loggy_show_info_all(): #show all loggy object in database
                 print(object.default_str)
         except:
             pass
+
+def loggy_show_loggy(loggy_list):
+    for object in loggy_list:
+        print(object.default_str)
 
 def loggy_search_date(date):
     #search from date input
@@ -36,13 +26,34 @@ def loggy_search_date(date):
                 object = pickle.load(file) 
                 if object.time.day == date[0] and object.time.month == date[1] and object.time.year == date[2]:
                     filtered_loggylist.append(object)
-        except:
-            pass
-    #search result
-    print("found :", len(filtered_loggylist))
-    time.sleep(3)
-    for object in filtered_loggylist:
-        print(object.default_str)
+        except: pass
+    return filtered_loggylist
+
+def loggy_search_datedur(datedur): #16-17/May/2015 
+    #search from date input
+    datedur = re.split('/', datedur)
+    datedur[0] = re.split('-', datedur[0])
+    print(datedur)
+    #check this date make sense
+    if datedur[0][0] > datedur[0][1]: 
+        return None
+    elif datedur[0][0] == datedur[0][1]:
+        loggy_search_date(datedur[0][0]+'/'+datedur[1]+'/'+datedur[2])
+        return None
+    #searching
+    filtered_loggylist = []
+    day_target = []
+    for i in range(int(datedur[0][0]), int(datedur[0][1])+1):
+        day_target.append(str(i))
+    print(day_target)
+    with open('class_loggy_obj.pickle', 'rb') as file: #read class loggy objects from file
+        try:
+            while True:
+                object = pickle.load(file) 
+                if object.time.day in day_target and object.time.month == datedur[1] and object.time.year == datedur[2]:
+                    filtered_loggylist.append(object)
+        except: pass
+    return filtered_loggylist
 
 def loggy_search_ip(ip):
     #searching
@@ -53,37 +64,73 @@ def loggy_search_ip(ip):
                 object = pickle.load(file) 
                 if object.ip.address == ip:
                     filtered_loggylist.append(object)
-        except:
-            pass
-    #search result
-    print("found :", len(filtered_loggylist))
-    time.sleep(3)
-    for object in filtered_loggylist:
-        print(object.default_str)
+        except: pass
+    return filtered_loggylist
 
-def loggy_search_timedu(start, stop): #16/May/2015_15:00:00 16/May/2015_16:00:00
-    start = re.split('_', start)
-    start_date = start[0]
-    start_time = start[1]
-    stop = re.split('_', stop)
-    stop_date = stop[0]
-    stop_time = stop[1]
+def loggy_database_append(loggy_list): #add loggy object to database
+    for object in loggy_list:
+        with open('class_loggy_obj.pickle', 'ab') as file:
+            pickle.dump(object, file)
 
-    start_date = re.split('/', start_date)
-    start_day = start_date[0]
-    start_month = start_date[1]
-    start_year = start_date[2]
-    start_time = re.split(':', start_time)
-    start_hour = start_time[0]
-    start_minute = start_time[1]
-    start_second = start_time[2]
+def loggy_database_rewrite(loggy_list):
+    for i in range(len(loggy_list)): #replace object in dummy list to database file
+        if i == 0:
+            with open('class_loggy_obj.pickle', 'wb') as file:
+                pickle.dump(loggy_list[i], file)
+        else:
+            with open('class_loggy_obj.pickle', 'ab') as file:
+                pickle.dump(loggy_list[i], file)
+                
+def loggy_read_log_file(path): #read log line put it into object loggy and save in database RETURN loggy_list
+    loggy_list = []
+    with open('apache_log.txt') as log:
+        count = len(open('apache_log.txt').readlines())
+        for i in range(count):
+            logline = log.readline()
+            temp = re.split('"', logline)
+            temp.remove(' ')
+            temp.pop()
 
-    stop_date = re.split('/', stop_date)
-    stop_day = stop_date[0]
-    stop_month = stop_date[1]
-    stop_year = stop_date[2]
-    stop_time = re.split(':', stop_time)
-    stop_hour = stop_time[0]
-    stop_minute = stop_time[1]
-    stop_second = stop_time[2]
+            #temp[0]-------------------------------------------
+            temp[0] = temp[0].replace('[','')
+            temp[0] = temp[0].replace(']','')
+            temp[0] = re.split(' ', temp[0])
+            temp[0][3] = re.split('/', temp[0][3])
+            temp[0][3][2] = re.split(':', temp[0][3][2])
+            temp[0].remove('')
+            
+            object = loggy()
+            object.default_str = logline
+            ip_add(temp[0][0]) #add to ip datbase auto check exist
+            object.ip = ip_get_object(temp[0][0])
+            object.identity = temp[0][1]
+            object.userID = temp[0][2]
+            object.time.day = temp[0][3][0]
+            object.time.month = temp[0][3][1]
+            object.time.year = temp[0][3][2][0]
+            object.time.hour = temp[0][3][2][1]
+            object.time.minute = temp[0][3][2][2]
+            object.time.second = temp[0][3][2][3]
+            object.time.zone = temp[0][4]
+            
+            #temp[1]----------------------------------
+            temp[1] = re.split(' ', temp[1])
+            object.http_req.method = temp[1][0]
+            object.http_req.path = temp[1][1]
+            object.http_req.protocol = temp[1][2]
 
+            #temp[2]----------------------------------
+            temp[2] = re.split(' ', temp[2])
+            temp[2].remove('')
+            object.status_code = temp[2][0]
+            object.return_size = temp[2][1]
+
+            #temp[3]----------------------------------
+            object.referer = temp[3]
+
+            #temp[4]----------------------------------
+            object.user_agent = temp[4]
+
+            loggy_list.append(object)
+
+    return(loggy_list)
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from loggy_collector import *
 
 help = """
 COMMANDS EXAMPLE
-log showall                 show all loggy in loggy database
-log search date <date>      search loggy by date (ex:16/May/2000)
-log search ip <ip>          search loggy by ip (ex:0.0.0.0)
-ip showall                  show all ip in ip database
-ip show <ip>                show specific ip in database    
+log showall                            show all loggy in loggy database
+log search date <date>                 search loggy by date (ex: 16/May/2000)
+log search datedur <dd-dd/mmm/yyyy>  search loggy by date during (ex: 16-17/May/2000)
+log search ip <ip>                     search loggy by ip (ex:0.0.0.0)
+log addlog <filepath>                  add new log file read and store in database
+ip showall                             show all ip in ip database
+ip show <ip>                           show specific ip in database    
 """
 
 while True:
@@ -16,16 +18,28 @@ while True:
     if cmd[0] == 'ip':
         try:
             if cmd[1] == "showall": ip_show_info_all()
-            elif cmd[1] == "show": ip_show_info(cmd[2])
+            elif cmd[1] == "show": 
+                try: ip_show_info(cmd[2]) 
+                except: pass
         except: pass
     elif cmd[0] == 'log':
         try:
             if cmd[1] == "showall": loggy_show_info_all()
-            elif cmd[1] == "search":
+            elif cmd[1] == "addlog": 
+                try: 
+                    loggy_database_append(loggy_read_log_file(cmd[2]))
+                except: pass
+            elif cmd[1] == "search": 
                 try:
-                    if cmd[2] == "date": loggy_search_date(cmd[3])
-                    elif cmd[2] == "ip": loggy_search_ip(cmd[3])
-                    elif cmd[2] == "timedu": loggy_search_timedu(cmd[3], cmd[4])
+                    if cmd[2] == "date": 
+                        try: loggy_show_loggy(loggy_search_date(cmd[3]))
+                        except: pass
+                    elif cmd[2] == "datedur": 
+                        try: loggy_show_loggy(loggy_search_datedur(cmd[3]))
+                        except: pass
+                    elif cmd[2] == "ip": 
+                        try: loggy_show_loggy(loggy_search_ip(cmd[3]))
+                        except: pass
                 except: pass
         except: pass
     elif cmd[0] == "exit": break
