4c02beadcd032d505032a8b7b0097fbe01b86167
best2000 <silentman@hotmail.co.th>
2019-12-20 12:15:44 +0700

    lol

--- test.py
+++ test.py
@@ -1,15 +1,33 @@
-import matplotlib.pyplot as plt; plt.rcdefaults()
-import numpy as np
+import tkinter as tk
 import matplotlib.pyplot as plt
-#NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from ip_collector import *
 
-x = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
-y = np.arange(len(x))
-performance = [10,8,6,4,2,1]
+ip = []
+rec = []
+loggylis = sort_by_visit_rec()
+for index, object in enumerate(loggylis):
+    if index == 9:
+        break
+    ip.append(object.address)
+    rec.append(object.visit_rec)
 
-plt.bar(y, performance, align='center', alpha=0.5)
-plt.xticks(y, x)
-plt.ylabel('Usage')
-plt.title('Programming language usage')
-
+"""
+plt.barh(ip, rec, color='green')
+plt.ylabel("IP")
+plt.xlabel("Visited records")
+plt.title("Top 10 IP Visited")
 plt.show()
+"""
+ip.reverse()
+rec.reverse()
+
+root= tk.Tk() 
+
+fig = plt.Figure(figsize=(6,5), dpi=100)
+pl = fig.add_subplot(111)
+pltk = FigureCanvasTkAgg(fig, root)
+pltk.get_tk_widget().grid(row=0, column=0)
+pl.barh(ip, rec, color='green')
+
+root.mainloop()
