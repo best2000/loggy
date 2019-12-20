9c01a987585151651f258cb84460d8340b4dfc78
best2000 <silentman@hotmail.co.th>
2019-12-21 03:25:54 +0700

    lol

--- test.py
+++ test.py
@@ -3,31 +3,44 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from ip_collector import *
 
-ip = []
-rec = []
 loggylis = sort_by_visit_rec()
-for index, object in enumerate(loggylis):
-    if index == 9:
-        break
-    ip.append(object.address)
-    rec.append(object.visit_rec)
+ip_rec = [[]]
+n = 1
+nex = 0
 
-"""
-plt.barh(ip, rec, color='green')
-plt.ylabel("IP")
-plt.xlabel("Visited records")
-plt.title("Top 10 IP Visited")
-plt.show()
-"""
-ip.reverse()
-rec.reverse()
+for i in range(len(loggylis)):
+    try:
+        if loggylis[i-1].visit_rec > loggylis[i].visit_rec:
+            n += 1
+            nex = 1
+    except: pass
+    if n == 11:
+        break
+    if nex == 1:
+        ip_rec.append([loggylis[i]])
+        nex = 0
+    else:
+        ip_rec[-1].append(loggylis[i])
+
+
+    
+top = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+rec = []
+for lis in ip_rec:
+    rec.append(lis[0].visit_rec)
 
 root= tk.Tk() 
 
 fig = plt.Figure(figsize=(6,5), dpi=100)
-pl = fig.add_subplot(111)
+pl = fig.add_subplot(111, title="Top 10 IP", xlabel="IP", ylabel="Visited")
+pl.bar(top, rec)
+
+
 pltk = FigureCanvasTkAgg(fig, root)
 pltk.get_tk_widget().grid(row=0, column=0)
-pl.barh(ip, rec, color='green')
+
+for i, lis in enumerate(ip_rec):
+    for object in lis:
+        print(i+1, ':', object.address)
 
 root.mainloop()
