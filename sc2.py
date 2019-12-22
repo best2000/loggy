c4fd63c74f6e60bc993633ca466c4bcd92927a7a
best2000 <silentman@hotmail.co.th>
2019-12-23 02:42:57 +0700

    lol

--- sc2.py
+++
@@ -1,8 +0,0 @@
-
-import socket               # Import socket module
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                 
-s.connect(("www.python.org" , 80))
-s.sendall(b'GET http://www.python.org HTTP/1.0\n\n')
-print (s.recv(4096))
-s.close()
